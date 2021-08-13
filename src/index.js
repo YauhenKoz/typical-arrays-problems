41c28d971ff8499aaa9a0ba808e449e646e01cf1
YauhenKoz <egkozel@gmail.com>
2021-08-13 18:16:34 +0300

    feat: add solution to js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
 exports.min = function min (array) {
-  return 0;
-}
+  if (array == null || array.length == 0) return 0;
+  let minNum = array.sort((a, b) => a - b);
+  return minNum[0];
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array == null || array.length == 0) return 0;
+  let maxNum = array.sort((a, b) => b - a);
+  return maxNum[0];
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+   if (array == null || array.length == 0) return 0;
+   let sum = 0;
+   for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+   };
+   return sum / array.length;
+};
